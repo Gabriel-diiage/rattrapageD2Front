fb735341a6464aecca80b1a2b04f16e2a20f6ef3
gabriel <gabriel.arnaud@diiage.org>
2025-08-25 23:36:53 +0200

    update

--- src/app/service/character-service.ts
+++ src/app/service/character-service.ts
@@ -11,9 +11,9 @@ export class CharacterService {
 
   private local_characters: Character[] = [
   new Character(1, 'Arnaud', 'Gab', 15, 'Développeur passionné par Angular'),
-  new Character(2, 'Marie', 'Durand', 28, 'Graphiste et illustratrice freelance'),
-  new Character(3, 'Jean', 'Martin', 45, 'Chef de projet dans le secteur IT'),
-  new Character(4, 'Sophie', 'Lemoine', 35, 'Experte en marketing digital'),
+  new Character(2, 'Garnier', 'Durand', 28, 'Graphiste et illustratrice freelance'),
+  new Character(3, 'Dupond', 'Martin', 45, 'Chef de projet dans le secteur IT'),
+  new Character(4, 'Lemoine', 'Lemoine', 35, 'Experte en marketing digital'),
   ];
 
   private apiUrl = 'https://localhost:7162/api/character';
